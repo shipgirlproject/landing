ae964539bf2779aef464b2200ab20ab116794ee9
0t4u <61939142+0t4u@users.noreply.github.com>
2023-12-08 15:13:33 +0000

    something

--- enhance.js
+++ enhance.js
@@ -1,3 +1,5 @@
+// we try to use as little js as possible
+
 document.addEventListener("DOMContentLoaded", () => {
     const copyright = document.getElementById("copyright")
     const copyrightText = copyright.innerText.replace("2018", new Date().getFullYear());
